a79b1505d40cdc2d19eb997dc13250e01de8429f
Kautuk Raj <spamkraj@gmail.com>
2019-08-14 19:42:57 +0530

    Add files via upload

--- Q4.py
+++ Q4.py
@@ -1,7 +1,11 @@
 # program to find hypotenuse
 import math
-a = float(input("Enter the first side"))
-b = float(input("Enter the second side"))
-c = math.sqrt(a*a + b*b)
-print("The hypotenuse is",c )
+a = float(input("Enter the first side "))
+b = float(input("Enter the second side "))
+if a>0 and b>0:
+ c = math.sqrt(a*a + b*b)
+ print("The hypotenuse is",c )
+
+else:
+    print ("Invalid input")
 
--- Q7.py
+++ Q7.py
@@ -1,8 +1,12 @@
 den =[200,50,10,5,2,1]
 amt = int(input("Enter the amount "))
 
-for i in range (6) :
-    count = int(amt / den[i]);
-    if count!=0 :
-        print(den[i], "x", count, "=" , den[i]*count)
-    amt = amt % den[i]    
+if amt>0 :
+    for i in range (6) :
+        count = int(amt / den[i]);
+        if count!=0 :
+            print(den[i], "x", count, "=" , den[i]*count)
+        amt = amt % den[i]
+
+else:
+    print ("Invalid input")
